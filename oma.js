5c2be35b788d3c39891f30e6de436c57a1b8c11e
kaislakudjoi <159912977+kaislakudjoi@users.noreply.github.com>
2024-11-21 22:33:44 +0200

    viimeistelyt valmis

--- oma.js
+++ oma.js
@@ -2,6 +2,23 @@ function laheta(){
     var nimi = document.getElementById("nimi").value;
     var sposti = document.getElementById("sposti").value;
     var puh = document.getElementById("puh").value;
+    var palaute = document.getElementById("palaute").value;
+    var parasta = document.querySelector('input[name="parasta"]:checked') ? document.querySelector('input[name="parasta"]:checked').value : '';
+    var toiveet = [];
+    var toiveetCheckboxes = document.querySelectorAll('input[name="lisaa"]:checked');
+    toiveetCheckboxes.forEach(function(checkbox) {
+        toiveet.push(checkbox.value);
+    });
+    var loytyiko = document.getElementById("löytyikö").value;
 
-    document.getElementById("vastaus").innerHTML= "Kiitos viestistä, " + nimi + " Sähköpostisi " + sposti + " Puhelinnumerosi " + puh;
+    var vastausHTML = `
+    <div class="vastaus-otsikko">Kiitos varauksestasi ${nimi} !</div>
+    <div class="vastaus-rivi"><span class="vastaus-teksti">Antamasi sähköposti:</span> ${sposti}, puhelin: ${puh}</div>
+    <div class="vastaus-rivi"><span class="vastaus-teksti">Palaute:</span> ${palaute}</div>
+    <div class="vastaus-rivi"><span class="vastaus-teksti">Parasta kahvilassamme on:</span> ${parasta}</div>
+    <div class="vastaus-rivi"><span class="vastaus-teksti">Toiveesi:</span> ${toiveet.join(', ') || 'Ei toiveita'}</div>
+    <div class="vastaus-rivi"><span class="vastaus-teksti">Löysitkö etsimäsi nettisivuilta?</span> ${loytyiko}</div>
+    `;
+
+    document.getElementById("vastaus").innerHTML = vastausHTML;
 }
